0521f37afb0036ee388c40c42a1f9bd8af145084
shlmt <155112268+shlmt@users.noreply.github.com>
2024-09-22 14:37:08 +0300

    return sendMail status

--- server/routes/mailRoute.js
+++ server/routes/mailRoute.js
@@ -4,10 +4,16 @@ const sendEmailToUser = require('../servises/mail');
 
 
 router.post('/', (req,res)=>{
+    const {to,title,html} = req.body
+    if(!to || !title || !html) 
+        return res.status(400).json({ msg: "חסרים שדות חובה" })
     try{
-        if(sendEmailToUser(req.body.to,req.body.title,req.body.html))
-            return res.json({ msg: "ההודעה נשלחה בהצלחה" })
-        else res.status(503).json({ msg: "שליחת ההודעה נכשלה" })
+        sendEmail(to,title,html).then((value)=>{
+            return res.status(201).json({ msg: "ההודעה נשלחה בהצלחה" })
+        },(reason)=>{
+            console.log("sendMail rejected",reason);
+            return res.status(503).json({ msg: "שליחת ההודעה נכשלה" })
+        })
     }
     catch(err){
         console.log(err)
